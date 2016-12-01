f95006d9b27996e7003526e83317b5c587d2e80d
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-11-30 17:54:39 -0700

    [list of atoms needed for calculation]

---
+++ sel_list.py
@@ -0,0 +1,7 @@
+##!/mnt/lustre_fs/users/mjmcc/apps/python2.7/bin/python
+##!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+sel = []
+sel.append(['resid 143 and name OG','resname GTP and name PA','resname GTP and name O3A'])
+
+
