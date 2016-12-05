c790edabc7a850646ce9bfde447a2714a7a48193
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-12-05 11:17:07 -0700

    [atom selections for calculating the specific angle]

--- sel_list.py
+++ sel_list.py
@@ -2,7 +2,7 @@
 ##!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 
 sel = []
-sel.append(['resid 143 and name OG','resname GTP and name PA','resname GTP and name O3A'])
-sel.append(['resid 143 and name OG','resname GTP and name PB','resname GTP and name O3B'])
+sel.append(['resid 143 and name OG','resname GTP and name PA','resname GTP and name O3A','OG_PA_O3A'])
+sel.append(['resid 143 and name OG','resname GTP and name PB','resname GTP and name O3B','OG_PB_O3B'])
 
 
